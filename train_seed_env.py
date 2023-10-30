bd8a8b7bb91a938e0bb225f5ffd709021a4169a9
Minjune Hwang <mjhwang@berkeley.edu>
2023-10-30 03:55:47 -0700

    Add submodules for robosuite and sb3 and add training scripts

---
+++ train_seed_env.py
@@ -0,0 +1,118 @@
+import os
+import sys
+import argparse
+import gym
+import numpy as np
+import torch as th
+
+sys.path.append(os.path.join(sys.path[0],'stable-baselines3'))
+sys.path.append(os.path.join(sys.path[0],'robosuite'))
+
+from stable_baselines3 import SEED
+
+import robosuite as suite
+from robosuite.environments.manipulation.POC_reaching import POCReaching
+from robosuite.wrappers import GymWrapper
+from robosuite import load_controller_config
+
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.callbacks import CallbackList, CheckpointCallback, EvalCallback, EveryNTimesteps
+
+def main(args):
+    if args.env == "Reaching2D":
+        obs_keys = ["eef_xyz", "gripper_state"]
+        total_timesteps = int(3e5)
+    elif args.env == "StackCustom":
+        obs_keys = ["cubeA_pos", "cubeB_pos"]
+        total_timesteps = int(1e6)
+    elif args.env == "Cleanup":
+        obs_keys = ["eef_xyz", "eef_yaw", "gripper_state", "pnp_obj_pos_yaw", "push_obj_pos_yaw"]
+        total_timesteps = int(4e6)
+    else:
+        raise ValueError()
+
+    env_train = suite.make(
+        env_name=args.env,
+        robots="Panda",
+        reward_scale=5.0,
+        horizon=1000,
+        control_freq=30,
+        use_skills=True,
+        use_delta=True,
+        use_yaw=False,
+        normalized_params=True,
+        use_aff_rewards=False,
+        normalized_obs=True,
+    )
+    env_train = GymWrapper(env_train, keys=obs_keys)
+
+    env_eval = suite.make(
+        env_name=args.env,
+        robots="Panda",
+        reward_scale=5.0,
+        horizon=1000,
+        control_freq=30,
+        use_skills=True,
+        use_delta=True,
+        use_yaw=False,
+        normalized_params=True,
+        use_aff_rewards=False,
+        normalized_obs=True,     
+    )
+    env_eval = GymWrapper(env_eval, keys=obs_keys)
+
+    eval_event = EvalCallback(
+        env_eval,
+        best_model_save_path=f"./logs/{args.env}/SEED_env/bs_{args.batch_size}_lr_{args.lr}_gamma_{args.gamma}_gs_{args.gsteps}/seed_{args.seed}/",
+        log_path=f"./logs/{args.env}/SEED_env/bs_{args.batch_size}_lr_{args.lr}_gamma_{args.gamma}_gs_{args.gsteps}/seed_{args.seed}/",
+        n_eval_episodes=10,
+        eval_freq=1,
+        deterministic=True,
+        render=False
+    )
+    eval_callback = EveryNTimesteps(n_steps=10000, callback=eval_event)
+    callback = CallbackList([eval_callback])
+
+    if args.save_checkpoint:
+        checkpoint_event = CheckpointCallback(
+            save_freq=1,
+            save_path=f"./logs/{args.env}/SEED_env/bs_{args.batch_size}_lr_{args.lr}_gamma_{args.gamma}_gs_{args.gsteps}/seed_{args.seed}/",
+            name_prefix="seed",
+            save_vecnormalize=True,
+        )
+        checkpoint_callback = EveryNTimesteps(n_steps=100000, callback=checkpoint_event)
+        callback.append(checkpoint_callback)
+
+    model_seed = SEED(
+        "MlpMixedPolicy",
+        env_train,
+        None,
+        action_dim_s=env_train.num_skills,
+        learning_rate=args.lr,
+        buffer_size=1000000,
+        learning_starts=0,
+        batch_size=args.batch_size,
+        tau=args.tau,
+        gamma=args.gamma,
+        gradient_steps=args.gsteps,
+        seed=args.seed,
+        use_env_feedback=True,
+        tensorboard_log=f"./logs/{args.env}/SEED_env/bs_{args.batch_size}_lr_{args.lr}_gamma_{args.gamma}_gs_{args.gsteps}/seed_{args.seed}/",
+    )
+    model_seed.learn(total_timesteps=total_timesteps, callback=callback, log_interval=4)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", type=str, default="Reaching2D")
+    parser.add_argument("--save-checkpoint", action="store_true")
+    parser.add_argument("--lr", type=float, default=3e-3)
+    parser.add_argument("--tau", type=float, default=0.005)
+    parser.add_argument("--batch-size", type=int, default=256)
+    parser.add_argument("--gamma", type=float, default=0.99)
+    parser.add_argument("--gsteps", type=int, default=5)
+    parser.add_argument("--reward-scale", type=float, default=5.0)
+    parser.add_argument("--seed", type=int, default=1)
+    args = parser.parse_args()
+    main(args)
+
---
+++ train_tamer.py
@@ -0,0 +1,111 @@
+import os
+import sys
+import argparse
+import gym
+import numpy as np
+import torch as th
+
+sys.path.append(os.path.join(sys.path[0],'stable-baselines3'))
+sys.path.append(os.path.join(sys.path[0],'robosuite'))
+
+from stable_baselines3 import SAC, TAMER
+
+import robosuite as suite
+from robosuite.environments.manipulation.POC_reaching import POCReaching
+from robosuite.wrappers import GymWrapper
+from robosuite import load_controller_config
+
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.callbacks import CallbackList, CheckpointCallback, EvalCallback
+
+def main(args):
+    if args.env == "Reaching2D":
+        obs_keys = ["eef_xyz", "gripper_state"]
+        total_timesteps = int(3e5)
+    elif args.env == "StackCustom":
+        obs_keys = ["cubeA_pos", "cubeB_pos"]
+        total_timesteps = int(2e6)
+    elif args.env == "Cleanup":
+        obs_keys = ["eef_xyz", "eef_yaw", "gripper_state", "pnp_obj_pos_yaw", "push_obj_pos_yaw"]
+        total_timesteps = int(4e6)
+
+    env_train = suite.make(
+        env_name=args.env,
+        robots="Panda",
+        reward_scale=5.0,
+        use_delta=True,
+        use_skills=False,
+        use_yaw=False,
+        normalized_params=True,
+        normalized_obs=True,
+    )
+    env_train = GymWrapper(env_train, keys=obs_keys)
+
+    env_eval = suite.make(
+        env_name=args.env,
+        robots="Panda",
+        reward_scale=5.0,
+        use_delta=True,
+        use_skills=False,
+        use_yaw=False,
+        normalized_params=True,
+        normalized_obs=True,
+    )
+    env_eval = GymWrapper(env_eval, keys=obs_keys)
+
+    # Save a checkpoint every 10000 steps
+    eval_callback = EvalCallback(
+        env_eval,
+        best_model_save_path=f"./logs/{args.env}/TAMER/bs_{args.batch_size}_lr_{args.lr}_gamma_{args.gamma}_gs_{args.gsteps}/seed_{args.seed}/",
+        log_path=f"./logs/{args.env}/TAMER/bs_{args.batch_size}_lr_{args.lr}_gamma_{args.gamma}_gs_{args.gsteps}/seed_{args.seed}/",
+        n_eval_episodes=10,
+        eval_freq=1000,
+        deterministic=True,
+        render=False
+    )
+    callback = CallbackList([eval_callback])
+
+    if args.save_checkpoint:
+        checkpoint_callback = CheckpointCallback(
+            save_freq=100000,
+            save_path=f"./logs/{args.env}/TAMER/bs_{args.batch_size}_lr_{args.lr}_gamma_{args.gamma}_gs_{args.gsteps}/seed_{args.seed}/",
+            name_prefix="tamer",
+            save_vecnormalize=True,
+        )
+        callback.append(checkpoint_callback)
+
+    model_sac = SAC.load(args.sac_checkpoint, env=env_train)
+    model_tamer = TAMER(
+        "HumanMlpPolicy",
+        env_train,
+        model_sac,
+        learning_rate=args.lr,
+        buffer_size=1000000,
+        learning_starts=0,
+        batch_size=args.batch_size,
+        tau=args.tau,
+        gamma=args.gamma,
+        gradient_steps=args.gsteps,
+        seed=args.seed,
+        q_val_threshold=0.999,
+        rl_threshold=0.0,
+        tensorboard_log=f"./logs/{args.env}/TAMER/bs_{args.batch_size}_lr_{args.lr}_gamma_{args.gamma}_gs_{args.gsteps}/seed_{args.seed}/",
+    )
+    model_tamer.learn(total_timesteps=total_timesteps, callback=callback, log_interval=4)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", type=str, default="Reaching2D")
+    parser.add_argument("--sac-checkpoint", type=str, default="logs/Reaching2D/SAC/bs_1024_lr_3e-05_gamma_0.99_gs_2/seed_2/best_model")
+    parser.add_argument("--save-checkpoint", action="store_true")
+    parser.add_argument("--lr", type=float, default=3e-4)
+    parser.add_argument("--tau", type=float, default=0.005)
+    parser.add_argument("--batch-size", type=int, default=256)
+    parser.add_argument("--gamma", type=float, default=0.99)
+    parser.add_argument("--gsteps", type=int, default=2)
+    parser.add_argument("--reward-scale", type=float, default=5.0)
+    parser.add_argument("--seed", type=int, default=1)
+    args = parser.parse_args()
+    main(args)
+
